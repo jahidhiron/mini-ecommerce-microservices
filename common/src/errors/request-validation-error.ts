ff0fd2f4cbc3321aafedc435b43106ecb86108df
Jahid Hiron <jahid.hiron@teamlance.io>
2024-03-07 07:39:29 +0600

    feat: add errors and middlewares to common module

---
+++ common/src/errors/request-validation-error.ts
@@ -0,0 +1,21 @@
+import {
+  FieldValidationError,
+  ValidationError,
+  Result,
+} from "express-validator";
+import { CustomError } from "./custom-error";
+
+export class RequestValidatorError extends CustomError {
+  statusCode = 400;
+  constructor(public errors: FieldValidationError[]) {
+    super("Invalid request parameters");
+
+    Object.setPrototypeOf(this, RequestValidatorError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((error) => {
+      return { message: error.msg, field: error.path };
+    });
+  }
+}
--- common/src/index.ts
+++ common/src/index.ts
@@ -1,13 +1,11 @@
-interface Color {
-  red: number;
-  green: number;
-  blue: number;
-}
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-connection-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
 
-const color: Color = {
-  red: 10,
-  green: 10,
-  blue: 15,
-};
-
-console.log(color);
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/requireAuth";
+export * from "./middlewares/validate-result";
